fe942f8b0441cabd1fe6439bdcc5233ed224b0e7
OSWALDO <oswaldodaniel.genis@gmail.com>
2022-06-08 07:53:18 -0500

    VARIABLES

---
+++ VARIABLES/app.js
@@ -0,0 +1,44 @@
+var nombre;
+nombre = "Oswaldo";
+var nombreUsuario ="Bomb";
+
+// STRINGS
+ var país = "Francia";
+ var fraseSecreta = "primer perrito"
+ var letra = "a";
+ var nombrePrincipal = "";
+
+ //Las variables se pueden reasignar sin que las pongamos de nuevo
+ país ="México";
+ letra ="x";
+
+ console.log ("hola")
+ console.log (fraseSecreta);
+
+ //number
+
+ var edad
+ edad =20;
+ var telefono = 111122223333
+ var costo = 20;
+ var resultado = -50;
+ var promedio = 9.87;
+ //las variables se pueden reasignar
+
+costo = 10;
+resultado =0;
+var numCelular = "23842834";
+//puede ser utilizado como strings texto numero como lo desee
+
+//concatenar es unir dos o mas strings
+console.log
+console.log(nombreUsuario + promedio);
+
+//boolean
+var esAdulto = true;
+var completado = false;
+var graduado = false;
+var cumple = true;
+var mexicano = false;
+
+//la resignación de valores no diferencía los tipos de datos
