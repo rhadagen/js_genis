a1bce22b05749cd1b456a75e7626812908f6023b
OSWALDO <oswaldodaniel.genis@gmail.com>
2022-06-08 01:27:40 -0500

    INTRODUCCIÓN A JS

---
+++ INTROJAVA/main.js
@@ -0,0 +1,31 @@
+//java es un lenguaje que se lee de arriba hacia abajo
+
+// SALIDAS
+
+// alerta desde java script
+alert ("quiuvo mundo")
+alert ( "alerta\nbbi")
+
+// Enviar mensajes a la consola
+console.log ("este mensaje se verá")
+console.log ("Mensaje")
+// Mostrar un error en la consola
+console.error ("Equivocación ")
+
+//Muestra un warning
+console.warn ("Cuidado, estás jugando con fuego")
+
+
+//ENTRADA
+prompt ("Introduce tu nombre")
+prompt ("Introduce tu apellido")
+
+document.write ("TEXTO DESDE JS")
+
+/*PARRAFO O BLOQUE
+1
+2
+3
+4
+5
+CERRAR Y TERMINAR COMENTARIO */
